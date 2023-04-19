841ea606a76774793a0a804bbf2a684c54e48580
jjd3109 <jongdoo2001@naver.com>
2023-04-19 14:06:11 +0900

    book테스트2

--- src/main/java/com/example/book/controller/TestController.java
+++ src/main/java/com/example/book/controller/TestController.java
@@ -4,15 +4,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class TestController {
 
     @GetMapping(value="/test")
-    public ResponseEntity<String> test(){
+    public ResponseEntity<Map> test(){
         String test = "테스트";
         String test2 = "테스트2";
 
-        return ResponseEntity.ok(test);
+        Map<String, Object> testMap = new HashMap<String, Object>();
+        testMap.put("1", test);
+        testMap.put("2", test2);
+
+
+        return ResponseEntity.ok(testMap);
     }
 
 }
